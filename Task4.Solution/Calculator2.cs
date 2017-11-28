d3ce4f2b68384b72788619fd280939e5d770ea85
ivinnichek <vinnichekira@gmail.com>
2017-11-28 11:20:50 +0300

    Add another method

---
+++ Task4.Solution/Calculator2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4.Solution
+{
+    public class EmptyClass
+    {
+        public double CalculateAverage(List<double> values, Func<List<double>, double> criteria)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return criteria(values);
+        }
+    }
+}
